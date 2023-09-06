7b46c49a965cb625f8e07003d665274c1133c770
erasky developer <erasky.developer@gmail.com>
2023-09-06 15:51:39 +0700

    feat: meta pixel code

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+var swiper1 = new Swiper(".swiping", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
